d9e218e47f063b0f633d2bf64fd98f75cb3ef077
Iftekher Aziz <azizchy02@gmail.com>
2023-06-02 19:54:35 +0600

    Install stripe, stripe for react and create card element

---
+++ bistro-boss-payment-client/src/pages/Dashboard/PaymentHistrory/CheckoutForm.jsx
@@ -0,0 +1,55 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  //
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+    
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      // Can't find a form element on the page.
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-primary btn-sm mt-4"
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
